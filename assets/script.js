0759b9fcfd11dc2c203f5ee0ba78bbb825069cca
continuouslearner24 <159856377+continuouslearner24@users.noreply.github.com>
2024-08-11 14:59:13 -0500

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+let blogItems = document.getElementById("blogItems");
+let personalBlogs = [];
+
+function saveBlogInStorage() {
+  let blogsFromStorage = localStorage.getItem("personalBlogs");
+  personalBlogs = JSON.parse(blogsFromStorage);
+  const newBlog = {};
+  if (document.getElementById("author").value != "" && document.getElementById("title").value != "" && document.getElementById("blogContent").value != "") {
+    newBlog.author = document.getElementById("author").value;
+    newBlog.title = document.getElementById("title").value;
+    newBlog.blogContent = document.getElementById("blogContent").value;
+    console.log(newBlog);
+    personalBlogs.push(newBlog);
+    console.log(personalBlogs);
+    // set content to local storage
+    localStorage.setItem("personalBlogs", JSON.stringify(personalBlogs));
+  } else {
+    alert("Please fill all of the fields!");
+  }
+  // redirect
+  document.location.href = "blog.html";
+}
+
+function showContent() {
+  let blogsFromStorage = localStorage.getItem("personalBlogs");
+  personalBlogs = JSON.parse(blogsFromStorage);
+  for (let i = 0; i < personalBlogs.length; i++) {
+    const newBlog = `<div class="newBlog"><h3>${personalBlogs[i].title}</h3><p>${personalBlogs[i].blogContent}</p><h6>- ${personalBlogs[i].author}</h6></div>`;
+    console.log(newBlog);
+    blogItems.innerHTML += newBlog;
+  }
+}
+
+showContent();
